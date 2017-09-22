6acabb0b7b1e653a0cefb2054addccb5e9f4a1cb
jenniferneale <jeneale6@gmail.com>
2017-09-22 16:42:21 -0400

    Made folders and files

---
+++ app.js
@@ -0,0 +1,7 @@
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
+// instance, which we'll want to use to add Markdown support later.
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
