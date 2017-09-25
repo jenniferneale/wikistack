061b2c35ad35fa2f425094cec583a6ec8f4811ae
jenniferneale <jeneale6@gmail.com>
2017-09-25 17:51:02 -0400

    Fixed indentations

--- models/index.js
+++ models/index.js
@@ -1,65 +1,65 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack'
-,{logging:false}
+    , { logging: false }
 );
 
 const Page = db.define('page', {
-	title:{
-	type:Sequelize.STRING,
-	allowNull:false
-	},
-	urlTitle: {
-	type:Sequelize.TEXT,
-	allowNull:false,
-	isURL:true
-	},
-	content: {
-	type:Sequelize.TEXT,
-	allowNull:false
-	},
-	status: {
-	type:Sequelize.ENUM('open', 'closed')
-	},
-	route: {
-		type:Sequelize.VIRTUAL,
-		get: function(){
-			return '/wiki/' + this.urlTitle;
-		}
-	},
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        isURL: true
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    },
+    route: {
+        type: Sequelize.VIRTUAL,
+        get: function () {
+            return '/wiki/' + this.urlTitle;
+        }
+    },
 },
-	{
-	hooks:{
-	beforeValidate: function(obj) {
-		if (obj.title) {
-			// Removes all non-alphanumeric characters from title
-			// And make whitespace underscore
-			obj.urlTitle = obj.title.replace(/\s+/g, '_').replace(/\W/g, '');
-		} else {
-			// Generates random 5 letter string
-			obj.urlTitle = Math.random().toString(36).substring(2, 7);//Should use a query later to have incremented number, not random!
-		}//end else
-	}//end function
-	}//end hook
-	}//end object
-	);
+    {
+        hooks: {
+            beforeValidate: function (obj) {
+                if (obj.title) {
+                    // Removes all non-alphanumeric characters from title
+                    // And make whitespace underscore
+                    obj.urlTitle = obj.title.replace(/\s+/g, '_').replace(/\W/g, '');
+                } else {
+                    // Generates random 5 letter string
+                    obj.urlTitle = Math.random().toString(36).substring(2, 7);//Should use a query later to have incremented number, not random!
+                }//end else
+            }//end function
+        }//end hook
+    }//end object
+);
 
 
 const User = db.define('user', {
-  name: {
-	type:Sequelize.STRING,
-	allowNull:false
-	},
-  email: {
-	type:Sequelize.STRING,
-	allowNull:false,
-	isEmail:true //Do we need a validation object here??
-	}
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        isEmail: true //Do we need a validation object here??
+    }
 })
 
-Page.belongsTo(User, {as: 'author'})
+Page.belongsTo(User, { as: 'author' })
 
 module.exports = {
-  db:db,
-  Page: Page,
-  User: User
+    db: db,
+    Page: Page,
+    User: User
 }
--- routes/wiki.js
+++ routes/wiki.js
@@ -3,49 +3,49 @@ var router = express.Router();
 module.exports = router;
 var models = require('../models');
 
-router.get('/',function(req,res,next){
-	res.redirect('/');
+router.get('/', function (req, res, next) {
+    res.redirect('/');
 });
 
-router.get('/add',function(req,res){
-	res.render('addpage');
+router.get('/add', function (req, res) {
+    res.render('addpage');
 });
 
-router.get('/:urlTitle',function(req,res,next){
-	models.Page.findOne({
-		where: {
-			urlTitle: req.params.urlTitle
-		}
-	})
-	.then(function(foundPage){
-    console.log("foundPage.keys " + Object.keys(foundPage));
-	res.render('wikipage',{title:foundPage.title,content:foundPage.content,authorName:foundPage.author});
-  })
-  .catch(next);
+router.get('/:urlTitle', function (req, res, next) {
+    models.Page.findOne({
+        where: {
+            urlTitle: req.params.urlTitle
+        }
+    })
+        .then(function (foundPage) {
+            console.log("foundPage.keys " + Object.keys(foundPage));
+            res.render('wikipage', { title: foundPage.title, content: foundPage.content, authorName: foundPage.author });
+        })
+        .catch(next);
 });
 
-router.post('/add',function(req,res){
-	if(req.body)
-	var user = models.User.findOrCreate({
-		where: {
-		name: req.body.author,
-		email: req.body.authorEmail
-		}
-	})
-	.then(function(user){
+router.post('/add', function (req, res) {
+    if (req.body)
+        var user = models.User.findOrCreate({
+            where: {
+                name: req.body.author,
+                email: req.body.authorEmail
+            }
+        })
+            .then(function (user) {
 
-		return models.Page.create({
-			title: req.body.title,
-			content: req.body.content,
-			status: req.body.pageStatus? "open" : "closed"
-		})
-		.then(function(page){
-			console.log(user)
-			return page.setAuthor(user[0])
-		})
-	})
+                return models.Page.create({
+                    title: req.body.title,
+                    content: req.body.content,
+                    status: req.body.pageStatus ? "open" : "closed"
+                })
+                    .then(function (page) {
+                        console.log(user)
+                        return page.setAuthor(user[0])
+                    })
+            })
 
-	.then(function(page){
-		res.redirect(page.urlTitle);
-	});
+            .then(function (page) {
+                res.redirect(page.urlTitle);
+            });
 });
